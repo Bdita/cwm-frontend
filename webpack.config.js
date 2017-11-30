e25800ec39110106f24a06b8c24903a57eec568c
Bdita <fpbandita@gmail.com>
2017-11-30 13:45:46 +1100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  devtool: 'eval-source-map',
+  entry:  __dirname + "/src/app/main.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015','react']
+        }
+      },
+      {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }
+    ]
+  },
+  devServer: {
+     contentBase: "./public",
+     historyApiFallback: true,
+     inline: true,
+     port: '3000'
+   }
+}
