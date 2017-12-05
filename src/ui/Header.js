0f15c1e4a6fa44f943df1a9038d0a2e266a5075a
Bdita <fpbandita@gmail.com>
2017-12-05 21:44:44 +1100

    drawer open and close

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,69 +1,96 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
 import constants from '../config/string-constants.json';
-import Face from 'material-ui/svg-icons/action/face';
+import FaceIcon from 'material-ui/svg-icons/action/face';
 import IconButton from 'material-ui/IconButton';
 import FlatButton from 'material-ui/FlatButton';
-import Booking from 'material-ui/svg-icons/action/today';
+import BookingIcon from 'material-ui/svg-icons/action/today';
+import ArrowDownIcon from 'material-ui/svg-icons/navigation/arrow-drop-down';
+import CloseIcon from 'material-ui/svg-icons/navigation/close';
+
 // import Logo from './Logo';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+    this.handleFaceButtonClick = this.handleFaceButtonClick.bind(this);
+  }
+
+  handleFaceButtonClick() {
+    this.setState({ open: !this.state.open });
+  }
+
   render() {
     return (
-      <AppBar
-        title={constants.title}
-        iconElementLeft={
-          <IconButton
-            iconStyle={{
-              width: 48,
-              height: 48,
-              marginTop: -12
-            }}
-            style={{
-              width: 48,
-              height: 48,
-            }}
-            disableTouchRipple={true}
-          >
-            <Face />
-          </IconButton>
-          }
-        iconElementRight={
-          <div style={{
-            marginTop: 0
-          }}
-          >
-            <FlatButton
-              label={constants.book}
-              labelPosition="before"
-              primary={true}
-              icon={<Booking color="#ED5F59" />}
+      <div>
+        <AppBar
+          title={constants.title}
+          iconElementLeft={
+            <IconButton
+              iconStyle={{
+                width: 48,
+                height: 48,
+                marginTop: -12
+              }}
               style={{
-                height: 40,
-                marginTop: 4,
+                width: 48,
+                height: 48,
               }}
-              labelStyle={{
-                color: '#767676',
-              }}
-            />
-          </div>
-        }
-        titleStyle={{
-          color: '#ED5F59',
-          fontFamily: 'Lora',
-          fontWeight: 'bold',
-          fontStyle: 'italic',
-          textAlign: 'center'
-        }}
-        style={{
-          marginBottom: '15px',
-          position: 'relative',
-          zIndex: '0',
-        }}
-        iconStyleLeft={{
-          color: '#767676'
-        }}
-      />
+              disableTouchRipple={true}
+              onClick={this.handleFaceButtonClick}
+            >
+              <FaceIcon />
+              <ArrowDownIcon />
+            </IconButton>
+            }
+          iconElementRight={
+            <div style={{
+              marginTop: 0
+            }}
+            >
+              <FlatButton
+                label={constants.book}
+                labelPosition="before"
+                primary={true}
+                icon={<BookingIcon color="#ED5F59" />}
+                style={{
+                  height: 40,
+                  marginTop: 4,
+                }}
+                labelStyle={{
+                  color: '#767676',
+                }}
+              />
+            </div>
+          }
+          titleStyle={{
+            color: '#ED5F59',
+            fontFamily: 'Lora',
+            fontWeight: 'bold',
+            fontStyle: 'italic',
+            textAlign: 'center'
+          }}
+          style={{
+            marginBottom: '15px',
+            position: 'relative',
+            zIndex: '0',
+          }}
+          iconStyleLeft={{
+            color: '#767676'
+          }}
+        />
+        <Drawer
+          open={this.state.open}
+          width="25%"
+        >
+          <IconButton onClick={this.handleFaceButtonClick}>
+            <CloseIcon />
+          </IconButton>
+          <p> Test </p>
+        </Drawer>
+      </div>
     );
   }
 }
