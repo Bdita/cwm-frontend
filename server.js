2886a4ee91ad6a998f7763cf1027b165f906b9ec
Bdita <fpbandita@gmail.com>
2018-01-07 01:46:36 +1100

    cors headers

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 // Since the root/src dir contains our index.html
 app.use(express.static(path.join(__dirname, '/build/')));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Heroku bydefault set an ENV variable called PORT=443
 //  so that you can access your site with https default port.
 // Falback port will be 8080; basically for pre-production test in localhost
